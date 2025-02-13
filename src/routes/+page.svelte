99619091e2bde90bb716938772a7b24c618c138d
Harish-Elangovan-10 <harishelango2005@gmail.com>
2025-02-13 14:55:26 +0530

    Navigation Bar Added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-zinc-100 h-screen flex items-center justify-center">
+    <div class="fixed top-0 h-20 w-full bg-gradient-to-br from-red-800 via-red-900 to-red-950">
+        <div class="flex items-center justify-between h-full px-4">
+            <div class="flex items-center">
+                <img src="https://img.freepik.com/premium-vector/ticket-icon-logo-vector-design-template_758102-2481.jpg?w=2000" alt="logo" class="h-12 w-12">
+                <h1 class="text-2xl text-white ml-2">TicketEase</h1>
+            </div>
+            <div class="flex items-center">
+                <a href="/login" class="text-white">Login</a>
+                <a href="/register" class="text-white ml-4">Register</a>
+            </div>
+        </div>
+    </div>
+  <h1 class="text-4xl text-black">Online ChatBot Ticketing System</h1>
+</div>
