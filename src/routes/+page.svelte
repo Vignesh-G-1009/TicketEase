2c96223120022ace3844f536346a07632b55ce79
Harish-Elangovan-10 <harishelango2005@gmail.com>
2025-02-14 14:56:03 +0530

    Home Page Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,31 @@
-<div class="bg-zinc-100 h-screen flex items-center justify-center">
-    <div class="fixed top-0 h-20 w-full bg-gradient-to-br from-red-800 via-red-900 to-red-950">
-        <div class="flex items-center justify-between h-full px-4">
-            <div class="flex items-center">
-                <img src="https://img.freepik.com/premium-vector/ticket-icon-logo-vector-design-template_758102-2481.jpg?w=2000" alt="logo" class="h-12 w-12">
-                <h1 class="text-2xl text-white ml-2">TicketEase</h1>
+<div class="bg-bgColor h-screen flex items-center justify-center">
+    <div class="fixed top-0 h-20 px-7 w-full bg-tileColor">
+        <div class="flex justify-between items-center h-full">
+            <div class="flex space-x-5 items-center">
+                <img class="h-16" src="https://static.vecteezy.com/system/resources/previews/018/931/148/original/cartoon-ticket-icon-png.png" alt="Logo" />
+                <h1 class="text-2xl bg-gradient-to-br from-yellow-500 via-orange-600 to-red-500 text-transparent bg-clip-text">TicketEase</h1>
             </div>
-            <div class="flex items-center">
-                <a href="/login" class="text-white">Login</a>
-                <a href="/register" class="text-white ml-4">Register</a>
+            <div class="flex space-x-7 items-center">
+                <a href="/login" class="text-xl bg-gradient-to-br from-blue-300 via-blue-500 to-blue-700 
+                    hover:bg-gradient-to-br hover:from-blue-500 hover:via-blue-700 hover:to-blue-900 text-transparent bg-clip-text">
+                    Login
+                </a>
+                <a href="/register" class="text-xl bg-gradient-to-br from-emerald-300 via-emerald-500 to-emerald-700 
+                    hover:bg-gradient-to-br hover:from-emerald-500 hover:via-emerald-700 hover:to-emerald-900 text-transparent bg-clip-text">
+                    Register
+                </a>
+            </div>
+        </div>
+        <div class="flex justify-evenly items-center py-20">
+            <div class="h-60 w-md bg-tileColor rounded-lg flex items-center justify-center text-white/15">
+                Museum 1
+            </div>
+            <div class="h-60 w-md bg-tileColor rounded-lg flex items-center justify-center text-white/15">
+                Museum 2
+            </div>
+            <div class="h-60 w-md bg-tileColor rounded-lg flex items-center justify-center text-white/15">
+                Museum 3
             </div>
         </div>
     </div>
-  <h1 class="text-4xl text-black">Online ChatBot Ticketing System</h1>
 </div>
