083bbeaf960a345cb8c3b3902d44ea9d305cf353
Harish-Elangovan-10 <harishelango2005@gmail.com>
2025-02-18 11:23:55 +0530

    Login page UI changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="bg-midnight h-screen flex items-center justify-center">
-    <div class="fixed top-0 h-20 px-7 w-full bg-lightGrey">
+    <div class="fixed top-0 h-20 px-7 w-full bg-darkGrey">
         <div class="flex justify-between items-center h-full">
             <div class="flex space-x-5 items-center">
                 <img class="h-16" src="https://static.vecteezy.com/system/resources/previews/018/931/148/original/cartoon-ticket-icon-png.png" alt="Logo" />
@@ -8,22 +8,22 @@
             <div class="flex space-x-7 items-center">
                 <a href="/login" class="text-xl bg-gradient-to-br from-blue-300 via-blue-500 to-blue-700 
                     hover:bg-gradient-to-br hover:from-blue-500 hover:via-blue-700 hover:to-blue-900 text-transparent bg-clip-text">
-                    Login
+                    Log In
                 </a>
-                <a href="/register" class="text-xl bg-gradient-to-br from-emerald-300 via-emerald-500 to-emerald-700 
+                <a href="/signup" class="text-xl bg-gradient-to-br from-emerald-300 via-emerald-500 to-emerald-700 
                     hover:bg-gradient-to-br hover:from-emerald-500 hover:via-emerald-700 hover:to-emerald-900 text-transparent bg-clip-text">
-                    Register
+                    Sign Up
                 </a>
             </div>
         </div>
         <div class="flex justify-evenly items-center py-20">
-            <div class="h-60 w-md bg-lightGrey rounded-lg flex items-center justify-center text-white/15">
+            <div class="h-60 w-md bg-darkGrey rounded-lg flex items-center justify-center text-white/15">
                 Museum 1
             </div>
-            <div class="h-60 w-md bg-lightGrey rounded-lg flex items-center justify-center text-white/15">
+            <div class="h-60 w-md bg-darkGrey rounded-lg flex items-center justify-center text-white/15">
                 Museum 2
             </div>
-            <div class="h-60 w-md bg-lightGrey rounded-lg flex items-center justify-center text-white/15">
+            <div class="h-60 w-md bg-darkGrey rounded-lg flex items-center justify-center text-white/15">
                 Museum 3
             </div>
         </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,132 @@
-<div class="bg-midnight h-screen flex items-center justify-center">
-    <div class="bg-lightGrey h-[50vh] w-[25vw] rounded-lg flex flex-col items-center justify-center text-white/15 space-y-10">
-        <div class="h-[12vh] w-[20vw] flex flex-col items-start justify-evenly">
-            <label for="Username" class="text-lg text-transparent bg-gradient-to-tr from-zinc-300 via-zinc-400 to-zinc-500 bg-clip-text">Username</label>
-            <input type="text" name="Username" id="Username" class="w-full px-3 py-2 border-2 border-white/15 rounded-lg focus:outline-none focus:border-transparent focus:ring-2 focus:ring-red-500">
-        </div>
-        <div class="h-[12vh] w-[20vw]">
-            <label for="Password" class="text-lg text-transparent bg-gradient-to-tr from-zinc-300 via-zinc-400 to-zinc-500 bg-clip-text">Password</label>
-        </div>
-    </div>
+<script lang="ts">
+	import { Mail, Lock, Eye, EyeClosed } from 'lucide-svelte';
+
+	let showPassword = false;
+	const togglePassword = () => {
+		showPassword = !showPassword;
+	}
+
+	let email = '';
+	let password = '';
+	const handleSubmit = (event: Event) => {
+		event.preventDefault();
+		console.log('Login: ', {email, password});
+	} 
+
+	const handleGoogleSignIn = () => {
+		console.log("Google Sign In Clicked");
+	}
+</script>
+
+<div class="min-h-screen text-white/75 bg-gradient-to-br from-gray-900 to-black flex items-center justify-center p-4">
+	<div class="flex flex-col items-center justify-stretch space-y-5">
+		<h1 class="h-12 text-4xl font-bold bg-gradient-to-r from-lime-400 to-emerald-400 bg-clip-text text-transparent">
+			Great to be back!
+		</h1>
+		<h2 class="text-gray-400 pb-3">
+			The faster you fuel up, the faster you snag your ticket.
+		</h2>
+		<div class="w-md bg-gradient-to-b from-gray-900 to-black rounded-xl shadow-[0_0_15px_rgba(0, 0, 0, 0.7)] p-8 border-[1.5px] border-gray-800 backdrop-blur-sm">
+			<form on:submit={handleSubmit}>
+				<div class="space-y-7">
+					<div class="relative group input-gradient">
+						<div class="absolute inset-y-0 l-0 pl-4 flex items-center pointer-events-none">
+							<Mail class="h-5 w-5 text-gray-400 group-focus-within:text-emerald-400 transition-colors" />
+						</div>
+						<input
+						type="email"
+						bind:value={email}
+						class="block w-full pl-12 pr-3 py-3 rounded-lg bg-gray-800/50 hover:bg-gray-800/80 focus:bg-gray-900/90 placeholder-white/30 transition-all duration-200 focus:outline-none" 
+						placeholder="Email Address"
+						required
+						/>
+					</div>
+					
+					<div class="relative group input-gradient">
+						<div class="absolute inset-y-0 l-0 pl-4 flex items-center pointer-events-none">
+							<Lock class="h-5 w-5 text-gray-400 group-focus-within:text-emerald-400 transition-colors" />
+						</div>
+						<input
+							type={showPassword ? "text" : "password"}
+							bind:value={password}
+							class="block w-full px-12 pr-3 py-3 rounded-lg bg-gray-800/50 hover:bg-gray-800/80 focus:bg-gray-900/90 placeholder-white/30 transition-all duration-200 focus:outline-none" 
+							placeholder="Password"
+							required
+						/>
+						<button 
+							type="button"
+							on:click={togglePassword}
+							class="absolute inset-y-0 right-0 pr-4 flex items-center cursor-pointers"
+						>
+							{#if showPassword}
+								<Eye class="w-5 h-5 text-gray-400" />
+							{:else}
+								<EyeClosed class="w-5 h-5 text-gray-400" />
+							{/if}
+						</button>
+					</div>
+
+					<div class="flex items-center justify-between">
+						<div class="flex items-center">
+							<input 
+								type="checkbox"
+								id="remember-me"
+								class="h-4 w-4"
+							/>
+							<label for="remenber-me" class="ml-2 block text-sm text-gray-400">
+								Remember me
+							</label>
+						</div>
+						<button
+							type="button"
+							class="text-sm bg-gradient-to-r from-lime-400 to-emerald-400 bg-clip-text text-transparent 
+							hover:from-lime-600 hover:to-emerald-600 focus:from-teal-500 focus:to-green-500 transition-all duration-200"
+						>
+							Forgot Password?
+						</button>
+					</div>
+
+					<button
+						type="submit"
+						class="w-full py-3 px-4 rounded-xl bg-gradient-to-br from-lime-500 to-emerald-500 hover:from-lime-600 hover:to-emerald-600
+						font-medium focus:outline-none focus:from-teal-500 focus:to-green-500 transition-all duration-200"
+					>
+						Sign In
+					</button>
+
+					<div class="relative">
+						<div class="absolute inset-0 pt-0.5 flex items-center">
+							<div class="w-full border-[1px] border-gray-800"></div>
+						</div>
+						<div class="relative flex justify-center text-sm">
+						  	<span class="px-2 bg-black text-gray-400">Or continue with</span>
+						</div>
+					</div>
+
+					<button
+						type="button"
+						on:click={handleGoogleSignIn}
+						class="flex items-center justify-center w-full py-3 px-4 rounded-xl bg-gradient-to-br from-gray-800 to-gray-900 hover:from-gray-900 hover:to-gray-950
+						font-medium focus:outline-none focus:from-gray-700 focus:to-gray-8s00 transition-all duration-200"
+					>
+						<img src="./src/assets/search.png" alt="Google" class="h-5 w-5 mr-3">
+						Sign in with Google
+					</button>
+				</div>				
+			</form>
+
+			<div class="mt-6 text-center">
+				<p>
+					Don't have an account?
+					<a
+						href="/signup"
+						class="pl-1 bg-gradient-to-r from-lime-400 to-emerald-400 bg-clip-text text-transparent 
+						hover:from-lime-600 hover:to-emerald-600 focus:from-teal-500 focus:to-green-500 transition-all duration-200"
+					>
+						Sign Up
+					</a>
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
